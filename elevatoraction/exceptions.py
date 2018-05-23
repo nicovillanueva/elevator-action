defca18b87438bb9f4988a40a7ba6eaccc9a7e53
Nico Villanueva <villanueva.arg@gmail.com>
2018-05-23 13:52:29 -0300

    clean

--- elevatoraction/exceptions.py
+++ elevatoraction/exceptions.py
@@ -2,9 +2,11 @@ class TooManyElevatorsError(Exception):
     """Command center overload!"""
     pass
 
+
 class DuplicatedElevatorError(Exception):
     """This elevator is already under the CC control"""
     pass
+
 
 class ElevatorNotFoundError(Exception):
     """Elevator ID not found within the control of this CC"""
--- elevatoraction/utils.py
+++ elevatoraction/utils.py
@@ -1,8 +1,10 @@
 def info(msg):
     print('[ INFO ] {}'.format(msg))
 
+
 def warn(msg):
     print('[ WARN ] {}'.format(msg))
+
 
 def err(msg):
     print('[ ERROR ] {}'.format(msg))
